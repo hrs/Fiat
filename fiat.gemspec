82b6e38b221e538c1022e9eeffbb5fb6d1bb7192
Harry Schwartz <hrs@cs.wm.edu>
2011-11-06 17:57:36 -0500

    Transmuting fiat into a gem.

--- fiat.gemspec
+++ fiat.gemspec
@@ -8,17 +8,15 @@ Gem::Specification.new do |s|
   s.authors     = ["Harry Schwartz"]
   s.email       = ["hrs@cs.wm.edu"]
   s.homepage    = ""
-  s.summary     = %q{TODO: Write a gem summary}
-  s.description = %q{TODO: Write a gem description}
+  s.summary     = %q{The "auto-Maker."}
+  s.description = %q{Autotest for Makefiles: fiat automatically and repeatedly executes a given make task every time one of the task's dependencies changes.}
 
   s.rubyforge_project = "fiat"
 
   s.files         = `git ls-files`.split("\n")
   s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
-  s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
+  s.executables   = ["fiat"]
   s.require_paths = ["lib"]
 
-  # specify any dependencies here; for example:
-  # s.add_development_dependency "rspec"
-  # s.add_runtime_dependency "rest-client"
+  s.add_dependency "colorize"
 end
--- lib/fiat.rb
+++ lib/fiat.rb
@@ -1,5 +1,62 @@
 require "fiat/version"
+require 'colorize'
 
 module Fiat
-  # Your code goes here...
+  def self.dep_tree
+    tree = {}
+    IO.readlines("Makefile").each do |line|
+      if line.include? ":"
+        k = line.split(":").first
+        line = line.split(":")[1..-1].join(":")
+        tree[k] = line.strip.split
+      end
+    end
+    tree
+  end
+
+  def self.deps(key, tree)
+    if tree.include? key
+      tree[key].map { |d| deps(d, tree) }.flatten.uniq
+    else
+      key
+    end
+  end
+
+  def self.file_ctimes(filenames)
+    hash = {}
+    filenames.each do |f|
+      if File.exists? f
+        hash[f] = File.ctime(f)
+      end
+    end
+    hash
+  end
+
+  def self.anything_changed?(filenames, ctimes)
+    ctimes != file_ctimes(filenames)
+  end
+
+  def self.passed_tests?(result_string)
+    $failing_words.each do |f|
+      if result_string.include? f
+        return false
+      end
+    end
+    true
+  end
+
+  def self.crashed?(process)
+    process.exitstatus != 0
+  end
+
+  def self.run_tests(dependencies, ctimes, instruction)
+    results = `make #{instruction}`
+    puts results + "\n"
+
+    if crashed?($?) or not passed_tests?(results)
+      puts ("#" * 40).red
+    else
+      puts ("#" * 40).green
+    end
+  end
 end
